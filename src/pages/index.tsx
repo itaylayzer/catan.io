edc0dc6f12d55f17c5b2a11c85b8f929ba57532a
itaylayzer <itaylayzer@gmail.com>
2025-05-03 17:46:19 +0300

    feat: home page skeleton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,96 @@
+import SVGWood from "@/components/svg/custom/SVGWood";
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { FaDoorOpen } from "react-icons/fa";
+import { FaKey } from "react-icons/fa";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import Catan2DSkeleton from "@/components/catan/map/Catan2DSkeleton";
+
 export function HomePage() {
-    return <h1>home page</h1>;
+    return (
+        <>
+            <div className="absolute z-20 flex flex-col gap-2 justify-center items-start top-0 left-50 w-[30%] h-full">
+                <Button
+                    variant="link"
+                    onClick={() => {
+                        window.open("https://itaylayzer.github.io/", "_blank");
+                    }}
+                    className="font-[Rubik] absolute bottom-8 left-0 cursor-pointer font-light"
+                >
+                    @itaylayzer
+                </Button>
+                <div className="flex gap-2 items-center">
+                    <SVGWood height={80} width={80} fill="#222" />
+                    <h1 className="text-8xl font-[Rubik] font-bold">Catan</h1>
+                </div>
+
+                <div className="flex gap-2">
+                    <Popover>
+                        <PopoverTrigger>
+                            <Button
+                                variant="link"
+                                className="cursor-pointer text-xl"
+                            >
+                                <FaDoorOpen /> host
+                            </Button>
+                        </PopoverTrigger>
+                        <PopoverContent>
+                            <div className="flex flex-col gap-3">
+                                <Input placeholder="enter name"></Input>
+                                <div className="flex justify-end">
+                                    <Button
+                                        variant="outline"
+                                        className="cursor-pointer"
+                                    >
+                                        <FaDoorOpen />
+                                        Host
+                                    </Button>
+                                </div>
+                            </div>
+                        </PopoverContent>
+                    </Popover>
+                    <Popover>
+                        <PopoverTrigger>
+                            <Button
+                                variant="link"
+                                className="cursor-pointer text-xl"
+                            >
+                                <FaKey /> join
+                            </Button>
+                        </PopoverTrigger>
+                        <PopoverContent>
+                            <div className="flex flex-col gap-3">
+                                <Input placeholder="enter name"></Input>
+                                <Input placeholder="enter code"></Input>
+                                <div className="flex justify-end">
+                                    <Button
+                                        variant="outline"
+                                        className="cursor-pointer"
+                                    >
+                                        <FaKey /> Join
+                                    </Button>
+                                </div>
+                            </div>
+                        </PopoverContent>
+                    </Popover>
+                </div>
+            </div>
+            <div className="z-0 absolute top-0 right-0 h-full w-[70%] overflow-hidden ">
+                <div className="top-0 right-0 absolute h-full w-full not-xl:scale-125 min-xl:scale-175 translate-x-100">
+                    <Catan2DSkeleton />
+                </div>
+            </div>{" "}
+        </>
+    );
 }
