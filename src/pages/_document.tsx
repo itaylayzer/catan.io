b34b42eee03295fcb06606f2aa3d4a457a64fa95
itaylayzer <itaylayzer@gmail.com>
2025-06-03 20:43:27 +0300

    del: sooner

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { Toaster } from "@/components/ui/sonner";
 export default function Document() {
     return (
         <Html lang="en">
@@ -10,12 +9,7 @@ export default function Document() {
             <body className="antialiased dark">
                 <TooltipProvider>
                     <Main />
-                    <Toaster
-                        expand
-                        richColors
-                        visibleToasts={20}
-                        theme="dark"
-                    />
+                   
                 </TooltipProvider>
                 <NextScript />
             </body>
