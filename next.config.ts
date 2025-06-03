0b091a985e75f51b7054d834f57fc3b1c0bb87a9
itaylayzer <itaylayzer@gmail.com>
2025-06-03 23:44:04 +0300

    fix: icon path

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,10 @@
 import type { NextConfig } from "next";
 
-const isExporting = (process.env.NEXT_EXPORT ?? "0") == "1";
-
-const distDir = isExporting ? "docs" : "out";
-const basePath = isExporting ? "/catan.io" : undefined;
-
 const nextConfig: NextConfig = {
     reactStrictMode: false,
 
     output: "export",
-    distDir,
-    basePath,
+    basePath: "/catan.io",
 };
 
 export default nextConfig;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { TooltipProvider } from "@/components/ui/tooltip";
+
 export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <link rel="icon" href="/icon.png" />
+                <link rel="icon" href="/catan.io/icon.png" />
             </Head>
             <body className="antialiased dark">
                 <TooltipProvider>
                     <Main />
-                   
                 </TooltipProvider>
                 <NextScript />
             </body>
